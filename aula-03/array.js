6bc3219bfe2c6e9e2e92637cdc635c3e9c4c6bfc
gcarniel <gpcarniel@outlook.com>
2021-02-03 21:07:50 -0300

    anotação ao final da aula 03.

---
+++ aula-03/array.js
@@ -0,0 +1,30 @@
+// ARRAYS
+    let heroes = ['Batman', 'Catwoman', 'Iron Man']
+    let ages = [10, 15, 23, 11, 31]
+
+    
+    for (let i in heroes){
+        console.log(heroes[i])
+    }
+    
+// METODOS DE ARRAY
+
+    // LENGTH
+    console.log('indices: ', heroes.length)
+
+    // JOIN - Junta os arrays, podendo passar como argumento algum caracter para separar os dados
+    console.log('join:', heroes.join(' - '))
+
+    // INDEXOF - Retorna o indice do dado passado como parametro, caso não haja retornará -1
+    console.log('indexOf: ', heroes.indexOf('Superman'))
+    
+    //CONCAT - Concatena um array a outro ou vários e não altera o array original
+    console.log('concat: ', heroes.concat(['Captain America', 'Thor']))
+    
+    // PUSH - Adiciona um elemento ao final do array - é mutável, ou seja, altera o array
+    heroes.push('Spider Man', 'Hulk')
+    console.log('push: ', heroes)
+
+    // POP - Remove o ultimo ele do array - é mutável
+    heroes.pop()
+    console.log('pop: ', heroes)
---
+++ aula-03/nullUndefined.js
@@ -0,0 +1,6 @@
+let emptiness
+
+console.log(emptiness, 'Undefined é quando uma variável não tem valor atribuído.')
+
+emptiness = null
+console.log(emptiness, 'Null é quando uma variável tem valor NULL atribuído intencionalmente.')
