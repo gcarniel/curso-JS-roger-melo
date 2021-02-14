892639f14cd47df0369f244ce396c1a15d487544
gcarniel <gpcarniel@outlook.com>
2021-02-14 17:47:33 -0300

    resolução dos exercícios.

--- etapa-05/exercicio-13/app.js
+++ etapa-05/exercicio-13/app.js
@@ -12,7 +12,9 @@ const getCatInfo = () => {
   const name = 'Marcos'
   let age = 3
   const color = 'Cinza'
+  return `${name} é um gato ${color} de ${age} anos.`
 }
+  console.log(getCatInfo())
 
 /*
   02
@@ -29,7 +31,9 @@ const external = () => {
     const extraInternal = () => {
       console.log(movie.toUpperCase())
     }
+    extraInternal()
   }
+  internal()
 }
 
 external()
@@ -44,6 +48,7 @@ external()
 */
 
 let randomNumbers = [3, 2, 1]
+console.log(randomNumbers.reverse())
 
 /*
   04
@@ -58,7 +63,8 @@ let crazyArray = [
   function getMessage () { return 'hi' },
   [ 5, 96, 53  ]
 ]
-
+crazyArray.shift()
+console.log(crazyArray[0]())
 /*
   05
 
@@ -75,6 +81,9 @@ const dogs = [
   { name: 'Zequinha', age: 7, gender: 'Male', breed: 'Poodle' },
   { name: 'Xica', age: 6, gender: 'Female', breed: 'Chihuahua' }
 ]
+
+  const findZequinha = dog => dog.name === 'Zequinha'
+  console.log(dogs.find(findZequinha))
 
 /*
   06
@@ -107,6 +116,8 @@ const dogs = [
     </article>
   </section>
 */
+  const title = document.querySelector('.main-title')
+  console.log(title)
 
 /*
   07
@@ -115,3 +126,6 @@ const dogs = [
     página, através da classe deles;
   - Exiba esse NodeList no console.
 */
+
+  const secondaryTitle = document.querySelectorAll('.secondary-title')
+  console.log(secondaryTitle)
